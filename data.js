c036d70a485d2ab28f8395f2ecee7e7e0bc0fafe
Raviraja09 <rajavysyaraju09@gmail.com>
2022-10-24 17:18:40 +0530

    code done

--- data.js
+++ data.js
@@ -1,1 +1,68 @@
-console.log('git')
+<!doctype html>
+
+<html>
+
+  <body>
+
+     
+
+    BOOK A CALL
+
+     
+
+     
+
+  </body>
+
+
+
+  <p>book a call slot and our representative will call you within 1hr of selected time </p>
+
+  <style>
+
+    form {
+
+    background-color:white;
+
+    text-align:center;
+
+    margin: 10px;
+
+    }
+
+    body {
+
+      background-color:white;
+
+      text-align:center;
+
+    }
+
+   </style>
+
+  <form>
+
+    <label>name</label>
+
+    <input type="text" name="username" required/><br>
+
+    <label>email</label>
+
+    <input type="email" name="email" required/><br>
+
+    <label>password</label>
+
+    <input type="password" name="password" required/><br>
+
+    <label>Time for call</label>
+
+    <input type="date" name="date" required/><br>
+
+    <button>Get a call</button>
+
+  </form>
+
+</body>
+
+</html>
+
