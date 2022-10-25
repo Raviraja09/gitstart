d069c0956189f31ce81f9c53606038e14012ba22
Raviraja09 <rajavysyaraju09@gmail.com>
2022-10-25 12:20:11 +0530

    manipulate the dom

--- .vscode/dom.js
+++ .vscode/dom.js
@@ -16,4 +16,26 @@ var submit=document.querySelector('input[type="submit"]');
 submit.value="send"
 var submit=document.querySelector('list-group-item');
 items.style.color='red';
+var itemlist=document.queryselector('#items');
+console.log(itemlist.parentnode);
+itemlist.parentnode.style.backgroundcolor='red';
+console.log(itemlist.parentnode.parentnode.parentnode);
+console.log(itemlist.parentelement);
+itemlist.parentelement.style.backgroundcolor='yellow';
+console.log(itemlist.parentelement.parentelement.parentelement);
+console.log(itemlist.child.nodes);
+console.log(itemlist.children);
+console.log(itemlist.children[1]);
+itemlist.children[1].style.backgroundcolor='yellow';
+console.log(itemlist.firstchild);
+console.log(itemlist.firstelementchild);
+var newdiv=document.createelement('div');
+newdiv.classname='hi';
+newdiv.id='hi1';
+newdiv.setattribute('title','hellodiv');
+var newdivtext=document.createtextnode('helloworld');
+console.log(newdiv);
+
+
+
 
