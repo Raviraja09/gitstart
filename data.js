12b7f8c6403537216d1409166d3f217f5db33199
Raviraja09 <rajavysyaraju09@gmail.com>
2022-10-25 09:16:14 +0530

    updated code

--- data.js
+++ data.js
@@ -1,68 +1,34 @@
-<!doctype html>
-
-<html>
-
-  <body>
-
-     
-
-    BOOK A CALL
-
-     
-
-     
-
-  </body>
-
-
-
-  <p>book a call slot and our representative will call you within 1hr of selected time </p>
-
-  <style>
-
-    form {
-
-    background-color:white;
-
-    text-align:center;
-
-    margin: 10px;
-
-    }
-
-    body {
-
-      background-color:white;
-
-      text-align:center;
-
-    }
-
-   </style>
-
-  <form>
-
-    <label>name</label>
-
-    <input type="text" name="username" required/><br>
-
-    <label>email</label>
-
-    <input type="email" name="email" required/><br>
-
-    <label>password</label>
-
-    <input type="password" name="password" required/><br>
-
-    <label>Time for call</label>
-
-    <input type="date" name="date" required/><br>
-
-    <button>Get a call</button>
-
-  </form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  <title>Item Lister</title>
+</head>
+<body>
+  <header id="main-header" class="bg-success text-white p-4 mb-3">
+    <div class="container">
+      <h1 id="header-title">Item Lister <span style="display:none">123</span></h1>
+    </div>
+  </header>
+  <div class="container">
+   <div id="main" class="card card-body">
+    <h2 class="title">Add Items</h2>
+    <form class="form-inline mb-3">
+      <input type="text" class="form-control mr-2">
+      <input type="submit" class="btn btn-dark" value="Submit">
+    </form>
+    <h2 class="title">Items</h2>
+    <ul id="items" class="list-group">
+      <li class="list-group-item">Item 1</li>
+      <li class="list-group-item">Item 2</li>
+      <li class="list-group-item">Item 3</li>
+      <li class="list-group-item">Item 4</li>
+    </ul>
+   </div>
+  </div>
 
 </body>
-
 </html>
-
